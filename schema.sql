550f28cc799fed9d5dc78984bc71f3b62eef7ee8
Osama Ali <mohd.osamaali@gmail.com>
2020-04-08 22:34:01 -0400

    Adding queries.sql and retirement_info.csv

--- schema.sql
+++ schema.sql
@@ -46,15 +46,18 @@ CREATE TABLE department_employees(
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
-	title VARCHAR (20) NOT NULL,
+	title VARCHAR (40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+PRIMARY KEY (emp_no, title, from_date)
 	);
 	
+SELECT * FROM titles;
+
+--DROP TABLE titles CASCADE;
