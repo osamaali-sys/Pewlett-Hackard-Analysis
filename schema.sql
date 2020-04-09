5b86dbc75eb0aa2921ab5d2ead3a02487348d7a8
Osama Ali <mohd.osamaali@gmail.com>
2020-04-09 13:37:08 -0400

    Adding tables before starting Module 7 challange with all skill drills

--- schema.sql
+++ schema.sql
@@ -61,3 +61,12 @@ PRIMARY KEY (emp_no, title, from_date)
 SELECT * FROM titles;
 
 --DROP TABLE titles CASCADE;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
